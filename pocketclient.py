f714763fb3842e1832fe34c1f0e7de6054d92c37
tshirtman <gabriel.pettier@gmail.com>
2014-04-03 01:56:06 +0200

    replace bottle with wsgiref
    better suited for serving only one request
    avoid using a thread
    make a simpler workflow
    
    add a callback param to send the results back to the calling app

--- pocketclient.py
+++ pocketclient.py
@@ -1,15 +1,15 @@
 import webbrowser
 import urllib2
 import json
-from bottle import route, run
+from wsgiref.simple_server import make_server
 #import osc
-import threading
-#from exceptions import Exception
 from pocket_keys import API_KEYS
+from random import randint
 
+PORT = randint(8080, 9090)
 
 BASE_URL = 'https://getpocket.com/'
-REDIRECT_URL = "http://localhost:8080/authorized/"
+REDIRECT_URL = "http://localhost:%s/authorized/" % PORT
 
 
 def get_token(platform):
@@ -23,33 +23,31 @@ def get_token(platform):
     return json.loads(result)["code"]
 
 
-def authorize(platform):
+def web_redirect(environ, start_response):
+    print "redirect called"
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ["app authorized, you can close this tab"]
+
+
+def authorize(platform, callback):
     code = get_token(platform)
     webbrowser.open(
         BASE_URL + 'auth/authorize?request_token=' + code +
-        '&redirect_uri=' + REDIRECT_URL
-    )
-
-    @route('/authorized/')
-    def web_redirect():
-        print "redirect called"
-        finish_authorize(platform, code)
-        return "app authorized, you can close this tab"
-    threading.Thread(target=run).run()
+        '&redirect_uri=' + REDIRECT_URL)
+    srv = make_server('localhost', PORT, web_redirect)
+    # we only want to serve once
+    srv.handle_request()
+    finish_authorize(platform, code, callback)
 
 
-def finish_authorize(platform, code):
+def finish_authorize(platform, code, callback):
     req = urllib2.Request(
         BASE_URL + 'v3/oauth/authorize',
         json.dumps({"consumer_key": API_KEYS[platform], "code": code}),
         {"Content-Type": "application/json", "X-Accept": "application/json"})
     result = json.loads(urllib2.urlopen(req).read())
     print result
-    save_token(result["access_token"], result["username"])
-
-
-def save_token(token, username):
-    pass
+    callback(API_KEYS[platform], result["access_token"], result["username"])
 
 
 def sync():
